9a245e6f381eb14a58629bfbef13c06b7e0e6f9c
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-06-08 14:12:17 +0530

    deconstructing and passing args to blocks some small changes

--- blog/migrations/0004_rename_field1.py
+++ blog/migrations/0004_rename_field1.py
@@ -13,22 +13,25 @@ def forward(apps, schema_editor):
     curr_field = BlogPage._meta.get_field(model_field_name)
     all_blocks = []
     for key, value in curr_field.stream_block.child_blocks.items():
+        path, args, kwargs = value.deconstruct()
         if key == new_name:
-            all_blocks.append((old_name, value.__class__()))
-            all_blocks.append((new_name, value.__class__()))
+            all_blocks.append((old_name, value.__class__(*args, **kwargs)))
+            all_blocks.append((new_name, value.__class__(*args, **kwargs)))
         else:
-            all_blocks.append((key, value.__class__()))
+            all_blocks.append((key, value.__class__(*args, **kwargs)))
     all_fields = StreamField(all_blocks)
     all_fields.contribute_to_class(BlogPage, model_field_name)
 
     # change the streamfield so that it has both blocks now
     schema_editor.alter_field(BlogPage, curr_field, all_fields)
 
-    for bp in BlogPage.objects.all():
-        for index, child in enumerate(getattr(bp, model_field_name)):
+    for page in BlogPage.objects.all():
+        stream_blocks = getattr(page, model_field_name)
+        for index, child in enumerate(stream_blocks):
             if child.block_type == old_name:
-                getattr(bp, model_field_name)[index] = (new_name, child.value)
-        bp.save()
+                stream_blocks[index] = (new_name, child.value)
+        setattr(page, model_field_name, stream_blocks)
+        page.save()
 
     # change the streamfield back to how it was
     schema_editor.alter_field(BlogPage, all_fields, curr_field)
@@ -43,22 +46,25 @@ def backward(apps, schema_editor):
     curr_field = BlogPage._meta.get_field(model_field_name)
     all_blocks = []
     for key, value in curr_field.clone().stream_block.child_blocks.items():
+        path, args, kwargs = value.deconstruct()
         if key == new_name:
-            all_blocks.append((old_name, value.__class__()))
-            all_blocks.append((new_name, value.__class__()))
+            all_blocks.append((old_name, value.__class__(*args, **kwargs)))
+            all_blocks.append((new_name, value.__class__(*args, **kwargs)))
         else:
-            all_blocks.append((key, value.__class__()))
+            all_blocks.append((key, value.__class__(*args, **kwargs)))
     all_fields = StreamField(all_blocks)
     all_fields.contribute_to_class(BlogPage, model_field_name)
 
     # change the streamfield so that it has both blocks now
     schema_editor.alter_field(BlogPage, curr_field, all_fields)
 
-    for bp in BlogPage.objects.all():
-        for index, child in enumerate(getattr(bp, model_field_name)):
+    for page in BlogPage.objects.all():
+        stream_blocks = getattr(page, model_field_name)
+        for index, child in enumerate(stream_blocks):
             if child.block_type == new_name:
-                getattr(bp, model_field_name)[index] = (old_name, child.value)
-        bp.save()
+                stream_blocks[index] = (old_name, child.value)
+        setattr(page, model_field_name, stream_blocks)
+        page.save()
 
     # change the streamfield back to how it was
     schema_editor.alter_field(BlogPage, all_fields, curr_field)
